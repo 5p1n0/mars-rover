09d77ac9498eb32e213e9d1f2b3b59a623c22b73
5p1n0 <lu.586@hotmail.it>
2023-02-09 10:54:54 +0100

    Update rover snapshot

--- packages/frontend/minimap/rover/rover.test.tsx
+++ packages/frontend/minimap/rover/rover.test.tsx
@@ -38,10 +38,16 @@ test("<RoverStyle />: snapshot test", () => {
       left: 5%;
       width: 90%;
       height: 90%;
-      background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='%230B171EFF' stroke-width='7' stroke-dasharray='16%2c 5%2c 9%2c 20' stroke-dashoffset='84' stroke-linecap='square'/%3e%3c/svg%3e");
+      background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='%230B171EFF' stroke-width='4' stroke-dasharray='50%25%2c 15%25' stroke-dashoffset='86' stroke-linecap='butt'/%3e%3c/svg%3e");
       border-radius: 100px;
       -webkit-animation: dRXZbQ 4s linear infinite,iAjNNh 2s ease-in-out;
       animation: dRXZbQ 4s linear infinite,iAjNNh 2s ease-in-out;
+    }
+
+    @media (min-width:43rem) {
+      .c0::before {
+        background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='%230B171EFF' stroke-width='6' stroke-dasharray='50%25%2c 15%25' stroke-dashoffset='86' stroke-linecap='butt'/%3e%3c/svg%3e");
+      }
     }
 
     <div
